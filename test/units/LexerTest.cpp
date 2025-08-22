d8bea496817f54232536e7ceb32fd2463bf6291d
Julius Schutte <Julius.Schutte@gmail.com>
2025-08-22 19:22:43 +0200

    Extended tests

--- test/units/LexerTest.cpp
+++ test/units/LexerTest.cpp
@@ -1,9 +1,15 @@
 #include <gtest/gtest.h>
 
 #include "Lexer.h"
-#include "Token.h"
 
-#include <format>
+/* I'm using weird helper functions bc I barely know how Google Test works.
+ * Many of the MATCHERS.* automatically print values passed to them upon failure.
+ * This is very annoying, especially when it tries to print types such as structs, resulting in binary gibberish.
+ * I couldn't find a way to get past that. Maybe with a TEST FIXTURE, but I kinda find those annoying.
+ * These helper functions do the job well enough. This isn't gonna be a real product anyway.
+ */
+
+typedef std::vector<lex::Token> TokenVector;
 
 static std::string tokenVariantToString(const lex::Token& token) {
   lex::TokenVariant tok_var = token.value;
@@ -21,55 +27,160 @@ static std::string tokenVariantToString(const lex::Token& token) {
   }
 
   return "ERROR: could not parse TokenVariant value to std::string";
-} 
-
-std::string getFullTokenStat(const lex::Token token) {
-  return std::format("Type: {}\nValue: {}\nLine: {}\nColumn: {}\n",
-      lex::literalTokenToString(token.type), tokenVariantToString(token), token.line, token.column);
 }
 
-/* For a string apparently the first column is the first character after a new line
- * So in this string "this is a string \nStart of new line"
- * the 't' from "this" is column 1 and then after the \n the
- * 'S' from "Start" is again the column 1
- * TODO: replace with this format
- * "string one\n"
- * "string literal two\n"
- * bc it looks better
- */
+void areTokensEqual(const lex::Token actual, const lex::Token expected) {
+    bool same_type = (actual.type == expected.type);
+    bool same_line = (actual.line == expected.line);
+    bool same_column = (actual.column == expected.column);
+
+    bool same_value = false;
+
+    std::visit([&same_value](auto&& arg1, auto&& arg2) {
+        using T1 = std::decay_t<decltype(arg1)>;
+        using T2 = std::decay_t<decltype(arg2)>;
+
+        if constexpr (std::is_same_v<T1, T2>) {
+            same_value = (arg1 == arg2);
+        } else {
+            same_value = false;
+        }
+    }, actual.value, expected.value);
+
+    bool result = same_type && same_value && same_line && same_column;
+
+    if (!result) {
+        std::string message = "Token mismatch: ";
+        
+        if (!same_type) {
+            message += "\n- Type mismatch: "
+            "\n  actual: " + lex::literalTokenToString(actual.type) +
+            "\n  expected: " + lex::literalTokenToString(expected.type) + '\n';
+        }
+        if (!same_value) {
+            message += "\n- Value mismatch: "
+            "\n  actual: " + tokenVariantToString(actual) +
+            "\n  expected: " + tokenVariantToString(expected) + '\n';
+        }
+        if (!same_line) {
+            message +=
+                "\n- Line mismatch: "
+                "\n actual: " + std::to_string(actual.line) +
+                "\n expected: " + std::to_string(expected.line) + '\n';
+        }
+        if (!same_column) {
+            message +=
+              "\n- Column mismatch: "
+              "\n actual: " + std::to_string(actual.column) +
+              "\n expected: " + std::to_string(expected.column) + '\n';
+        }
+        ADD_FAILURE();
+        std::cout << message << "\n\n";
+    }
+}
+
+void compareTokenStream(lex::Lexer& lexer, const TokenVector expected_tokens) {
+    lex::Token token = lexer.nextToken();
+    size_t index{0};
+
+    while (true) {
+      areTokensEqual(token, expected_tokens.at(index));
+      token = lexer.nextToken();
+
+      if (token.type == lex::SIS_EOF) break;
+      ++index;
+    }
+}
+
+TEST(Lexer, SkipComments) {
+  std::string input =
+    "class DaClass /* Random comment */ {};\n"
+    "pin this_variable = //line comment getting in the way \" value of variable \""
+    ;
+
+  const TokenVector expected = {
+    {.type = lex::CLASS,     .value = {}, .line = 1, .column = 1},
+    {.type = lex::IDENT,     .value = "DaClass", .line = 1, .column = 7},
+    {.type = lex::L_BRACE,   .value = {}, .line = 1, .column = 36},
+    {.type = lex::R_BRACE,   .value = {}, .line = 1, .column = 37},
+    {.type = lex::SEMICOLON, .value = {}, .line = 1, .column = 38},
+
+    {.type = lex::PIN,     .value = {}, .line = 2, .column = 1},
+    {.type = lex::IDENT,   .value = "this_variable", .line = 2, .column = 5},
+    {.type = lex::ASSIGN,  .value = {}, .line = 2, .column = 19},
+    {.type = lex::SIS_EOF, .value = {}, .line = 2, .column = 87},
+  };
+
+  lex::Lexer lexer = lex::Lexer(input);
+  compareTokenStream(lexer, expected);
+}
+
+TEST(Lexer, LexerReusabilityWorks) {
+  std::string first_input = "pin var1 = 20;";
+  std::string second_input = "if (true) {/* comment */}";
+
+  const std::vector<lex::Token> set_one_expected_tokens {
+    {.type = lex::PIN,       .value = {},     .line = 1, .column = 1  },
+    {.type = lex::IDENT,     .value = "var1", .line = 1, .column = 5  },
+    {.type = lex::ASSIGN,    .value = {},     .line = 1, .column = 10 },
+    {.type = lex::NUM,       .value = 20.,    .line = 1, .column = 12 },
+    {.type = lex::SEMICOLON, .value = {},     .line = 1, .column = 14 },
+    {.type = lex::SIS_EOF,   .value = {},     .line = 1, .column = 15 },
+  };
+  const std::vector<lex::Token> set_two_expected_tokens {
+    {.type = lex::IF,      .value = {}, .line = 1, .column = 1  },
+    {.type = lex::L_PAREN, .value = {}, .line = 1, .column = 4  },
+    {.type = lex::TRUE,    .value = {}, .line = 1, .column = 5  },
+    {.type = lex::R_PAREN, .value = {}, .line = 1, .column = 9  },
+    {.type = lex::L_BRACE, .value = {}, .line = 1, .column = 11 },
+    {.type = lex::R_BRACE, .value = {}, .line = 1, .column = 25 },
+    {.type = lex::SIS_EOF, .value = {}, .line = 1, .column = 26 },
+  };
+
+  lex::Lexer lexer(first_input);
+
+  compareTokenStream(lexer, set_one_expected_tokens);
+
+  lexer.reset();
+  lexer.newInput(second_input);
+
+  compareTokenStream(lexer, set_two_expected_tokens);
+}
+
 TEST(Lexer, CorrectTokenSplits) {
   const std::string input =
-       /* 12345678901234567890123456789012345678901234567890*/
-/* 1 */  "pin int_literal = 5;\n"
-/* 2 */  "pin double_literal = 5.5;\n"
-/* 3 */  "pin str_literal = \"false true \\n\\r\"\n"
-/* 4 */  "{true, false, null}\n"
-/* 5 */  "else for while switch case\n"
-/* 6 */  "\n"
-/* 7 */  "if (true) {\n"
-/* 8 */  "    funcCall();\n"
-/* 9 */  "    wait();\n"
-/*10 */  "} else {\n"
-/*11 */  "    otherFuncCall();\n"
-/*12 */  "}\n"
-/*13 */  "\n"
-/*14 */  "return break continue\n"
-/*15 */  "fn name(par1, par2) {};\n"
-/*16 */  "class ClassName {};\n"
-/*17 */  "#include \"this is a string literal\"\n"
-/*18 */  "num++ ++num --num num--\n"
-/*19 */  "* / %\n"
-/*20 */  "= += -= *= /= %=\n"
-/*21 */  "== != < <= > >= && || !\n"
-/*22 */  "() {} [] <>\n"
-/*23 */  ", . : :: ; ->\n"
-/*24 */  "// comment line\n"
-/*25 */  "/* comment block\n"
-/*26 */  " * comment block\n"
-/*27 */  " */\n"
-/*28 */  "// TODO: suffer\n"
-/*29 */  "THIS_IS_A_TEST_IDENTIFIER\n"
-/*30 */  "/* This is a random comment */";
+    /* 12345678901234567890123456789012345678901234567890*/
+    /* 1 */  "pin int_literal = 5;\n"
+    /* 2 */  "pin double_literal = 5.5;\n"
+    /* 3 */  "pin str_literal = \"false true \\n\\r\"\n"
+    /* 4 */  "{true, false, null}\n"
+    /* 5 */  "else for while switch case\n"
+    /* 6 */  "\n"
+    /* 7 */  "if (true) {\n"
+    /* 8 */  "    funcCall();\n"
+    /* 9 */  "    wait();\n"
+    /*10 */  "} else {\n"
+    /*11 */  "    otherFuncCall();\n"
+    /*12 */  "}\n"
+    /*13 */  "\n"
+    /*14 */  "return break continue\n"
+    /*15 */  "fn name(par1, par2) {};\n"
+    /*16 */  "class ClassName {};\n"
+    /*17 */  "#include \"this is a string literal\"\n"
+    /*18 */  "num++ ++num --num num--\n"
+    /*19 */  "* / %\n"
+    /*20 */  "= += -= *= /= %=\n"
+    /*21 */  "== != < <= > >= && || !\n"
+    /*22 */  "() {} [] <>\n"
+    /*23 */  ", . : :: ; ->\n"
+    /*24 */  "// comment line\n"
+    /*25 */  "/* comment block\n"
+    /*26 */  " * comment block\n"
+    /*27 */  " */\n"
+    /*28 */  "// TODO: suffer\n"
+    /*29 */  "THIS_IS_A_TEST_IDENTIFIER\n"
+    /*30 */  "/* This is a random comment */"
+             ;
 
   static const std::vector<lex::Token> expected_tokens = {
     {.type = lex::PIN           , .value = {}              , .line = 1  , .column = 1  },
@@ -205,22 +316,15 @@ TEST(Lexer, CorrectTokenSplits) {
     {.type = lex::SIS_EOF       , .value = {}              ,  .line = 30, .column = 31 }
   };
 
-lex::Lexer* lexer = new lex::Lexer(input);
-int index{0};
-while (true) {
-  const lex::Token actual = lexer->nextToken();
-  const lex::Token& expected = expected_tokens.at(index);
+  lex::Lexer* lexer = new lex::Lexer(input);
+  int index{0};
+  while (true) {
+    const lex::Token actual = lexer->nextToken();
+    const lex::Token& expected = expected_tokens.at(index);
 
-  std::string actual_val_str = tokenVariantToString(actual);
-  std::string expected_val_str = tokenVariantToString(expected);
+    areTokensEqual(actual, expected);
 
-  ASSERT_EQ(lex::literalTokenToString(actual.type), lex::literalTokenToString(expected.type)) << "\nExpected:\n" << getFullTokenStat(expected) << "\nGot:\n" << getFullTokenStat(actual);
-  ASSERT_EQ(actual_val_str, expected_val_str) << "\nExpected:\n" << getFullTokenStat(expected) << "\nGot:\n" << getFullTokenStat(actual);
-  ASSERT_EQ(actual.line, expected.line) << "\nExpected:\n" << getFullTokenStat(expected) << "\nGot:\n" << getFullTokenStat(actual);
-  ASSERT_EQ(actual.column, expected.column) << "\nExpected:\n" << getFullTokenStat(expected) << "\nGot:\n" << getFullTokenStat(actual);
-
-  if (actual.type == lex::SIS_EOF) break;
-
-  index++;
-}
+    if (actual.type == lex::SIS_EOF) break;
+    index++;
+  }
 }
