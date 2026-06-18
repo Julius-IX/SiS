0880f77ffdc352b3773fba35e063941cb3ec60e1
Julius Schutte <Julius.Schutte@gmail.com>
2026-06-18 21:23:17 +0200

    feat: update base lexture functions

--- include/Parser.h
+++ include/Parser.h
@@ -2,21 +2,31 @@
 
 #include <Lexer.h>
 #include <vector>
+#include <ParserNodeTypes.h>
 
 namespace par {
   class Parser {
     public:
-    Parser() = default;
+    Parser() = delete;
     ~Parser() = default;
-
-    void parse(lex::Lexer* lexer);
 
     private:
     lex::Lexer* m_lexer;
     std::vector<lex::Token> m_tokens;
+    Block m_root;
 
-    void parseLiteral(const lex::Token& token);
-    std::string formatIllegalTokenMessage(const lex::Token& token);
+    std::string formatIllegalTokenMessage(const lex::Token& token, std::string_view msg = "");
+
+    std::unique_ptr<Node> parseLiteral(lex::Lexer* lexer);
+    std::unique_ptr<Node> parseIdentifier(lex::Lexer* lexer);
+    std::unique_ptr<Node> parseUnary(lex::Lexer* lexer);
+    std::unique_ptr<Node> parseBinary(lex::Lexer* lexer);
+    std::unique_ptr<Node> parseBlock(lex::Lexer* lexer);
+    std::unique_ptr<Node> parseIf(lex::Lexer* lexer);
+    std::unique_ptr<Node> parseWhile(lex::Lexer* lexer);
+    std::unique_ptr<Node> parseVarDecl(lex::Lexer* lexer);
+    std::unique_ptr<Node> parseExprStmt(lex::Lexer* lexer);
+    std::unique_ptr<Node> parseCall(lex::Lexer* lexer);
   };
 
 } // namespace par
--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,94 +1,43 @@
-#include <Parser.h>
 #include <Logging.h>
+#include <Parser.h>
+#include <ParserNodeTypes.h>
+#include <Token.h>
+#include <print>
+
+// NOTE: placeholder
+static void panic(const std::string_view msg) { std::print("PANIC: {}\n", msg.data()); }
 
 namespace par {
-  void Parser::parse(lex::Lexer* lexer) {
-    m_lexer = lexer;
-    lex::Token* current_token = nullptr;
-
-    // big ass switch statement to handle all the different token types ಥ_ಥ
-    while (current_token->type != lex::TokenType::SIS_EOF) {
-      m_tokens.push_back(m_lexer->nextToken());
-      current_token = &m_tokens.back();
-
-      switch (current_token->type) {
-        case lex::TokenType::ILLEGAL: LOG_ERROR(formatIllegalTokenMessage(*current_token)); break;
-        case lex::TokenType::IDENT: break;
-
-        case lex::TokenType::NUM:
-        case lex::TokenType::STRING:
-        case lex::TokenType::TRUE:
-        case lex::TokenType::FALSE:
-        case lex::TokenType::SIS_NULL: parseLiteral(*current_token); break;
-
-        case lex::TokenType::IF: break;
-        case lex::TokenType::ELSE: break;
-        case lex::TokenType::FOR: break;
-        case lex::TokenType::WHILE: break;
-        case lex::TokenType::SWITCH: break;
-        case lex::TokenType::CASE: break;
-        case lex::TokenType::RETURN: break;
-        case lex::TokenType::BREAK: break;
-        case lex::TokenType::CONTINUE: break;
-
-        case lex::TokenType::FN: break;
-        case lex::TokenType::PIN: break;
-        case lex::TokenType::CLASS: break;
-        case lex::TokenType::INCLUDE: break;
-
-        case lex::TokenType::PLUS: break;
-        case lex::TokenType::PLUS_PLUS: break;
-        case lex::TokenType::MINUS: break;
-        case lex::TokenType::MINUS_MINUS: break;
-        case lex::TokenType::STAR: break;
-        case lex::TokenType::SLASH: break;
-        case lex::TokenType::PERCENT: break;
-        case lex::TokenType::ASSIGN: break;
-        case lex::TokenType::PLUS_ASSIGN: break;
-        case lex::TokenType::MINUS_ASSIGN: break;
-        case lex::TokenType::STAR_ASSIGN: break;
-        case lex::TokenType::SLASH_ASSIGN: break;
-        case lex::TokenType::PERCENT_ASSIGN: break;
-
-        case lex::TokenType::EQUALS: break;
-        case lex::TokenType::NOT_EQUALS: break;
-        case lex::TokenType::LESS_THAN: break;
-        case lex::TokenType::LESS_THAN_EQUALS: break;
-        case lex::TokenType::GREATER_THAN: break;
-        case lex::TokenType::GREATER_THAN_EQUALS: break;
-        case lex::TokenType::AND: break;
-        case lex::TokenType::OR:
-        case lex::TokenType::NOT: break;
-
-        case lex::TokenType::L_PAREN: break;
-        case lex::TokenType::R_PAREN: break;
-        case lex::TokenType::L_BRACK: break;
-        case lex::TokenType::R_BRACK: break;
-        case lex::TokenType::L_BRACE: break;
-        case lex::TokenType::R_BRACE: break;
-
-        case lex::TokenType::COMMA: break;
-        case lex::TokenType::DOT: break;
-        case lex::TokenType::COLON: break;
-        case lex::TokenType::SCOPE_RES: break;
-        case lex::TokenType::SEMICOLON: break;
-        case lex::TokenType::HASH: break;
-        case lex::TokenType::ARROW: break;
-
-        case lex::TokenType::COMMENT: break;
-        default: break;
-      }
+  std::string Parser::formatIllegalTokenMessage(const lex::Token& token, const std::string_view msg) {
+    std::string fmted_msg = fmt::format("Illegal token received {}:{}\n", token.line, token.column);
+    if (!msg.empty()) {
+      fmted_msg += msg;
+      fmted_msg += "\n";
     }
-    m_lexer = nullptr;
+    return fmted_msg +
+           fmt::format("{}\n{}\n", m_lexer->getLineContent(token.line), std::string(token.column - 1, ' ') + std::string(m_lexer->peekToken().column - token.column, '^'));
+  }
+  static bool isAssignmentOperator(lex::TokenType type) {
+    switch (type) {
+      case lex::TokenType::ASSIGN:
+      case lex::TokenType::PLUS_ASSIGN:
+      case lex::TokenType::MINUS_ASSIGN:
+      case lex::TokenType::STAR_ASSIGN:
+      case lex::TokenType::SLASH_ASSIGN:
+      case lex::TokenType::PERCENT_ASSIGN: return true;
+      default: return false;
+    }
   }
 
-  std::string Parser::formatIllegalTokenMessage(const lex::Token& token) {
-    return fmt::format(
-      "Illegal token received {}:{}\n{}\n{}",
-      token.line,
-      token.column,
-      m_lexer->getLineContent(token.line),
-      std::string(token.column - 1, ' ') + std::string(m_lexer->peekToken().column - token.column, '^')
-    );
-  }
+  std::unique_ptr<Node> Parser::parseLiteral(lex::Lexer* lexer) { return nullptr; }
+  std::unique_ptr<Node> Parser::parseIdentifier(lex::Lexer* lexer) { return nullptr; }
+  std::unique_ptr<Node> Parser::parseUnary(lex::Lexer* lexer) { return nullptr; }
+  std::unique_ptr<Node> Parser::parseBinary(lex::Lexer* lexer) { return nullptr; }
+  std::unique_ptr<Node> Parser::parseBlock(lex::Lexer* lexer) { return nullptr; }
+  std::unique_ptr<Node> Parser::parseIf(lex::Lexer* lexer) { return nullptr; }
+  std::unique_ptr<Node> Parser::parseWhile(lex::Lexer* lexer) { return nullptr; }
+  std::unique_ptr<Node> Parser::parseVarDecl(lex::Lexer* lexer) { return nullptr; }
+  std::unique_ptr<Node> Parser::parseExprStmt(lex::Lexer* lexer) { return nullptr; }
+  std::unique_ptr<Node> Parser::parseCall(lex::Lexer* lexer) { return nullptr; }
+
 } // namespace par
