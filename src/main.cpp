13fceb72c922b862bed5f6fcd91b16f8f5c187f9
Julius Schutte <Julius.Schutte@gmail.com>
2026-06-19 16:17:19 +0200

    feat: add minimal main

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,19 @@
+#include <Evaluator.h>
+#include <Lexer.h>
+#include <Parser.h>
+#include <Token.h>
+#include <print>
+
 int main(const int argc, const char* argv[]) {
-    // NOTE: assume given argument is path to valid .sis file
+  if (argc < 1) {
+    std::print("usage: SiS <path/to/file.sis>");
+  }
+
+  par::Parser parser;
+  if (!parser.parseFile(argv[1])) return 1;
+
+  eval::Evaluator evaluator;
+  evaluator.run(*parser.getRoot());
+
+  return 0;
 }
