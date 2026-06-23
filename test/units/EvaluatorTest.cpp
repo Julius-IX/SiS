5221e10fc6bc4f01178acdd28d23470948946862
Julius Schutte <Julius.Schutte@gmail.com>
2026-06-23 09:46:58 +0200

    test(eval): add test for literals

--- test/units/EvaluatorTest.cpp
+++ test/units/EvaluatorTest.cpp
@@ -35,3 +35,27 @@ static eval::Value runScript(const std::string& source) {
   return t.run();
 }
 
+namespace { // Literals
+
+  TEST(Evaluator, NumberLiteral) {
+    auto v = runScript("42;");
+    ASSERT_TRUE(std::holds_alternative<double>(v.data));
+    EXPECT_DOUBLE_EQ(std::get<double>(v.data), 42.0);
+  }
+
+  TEST(Evaluator, StringLiteral) {
+    auto v = runScript(R"("hello";)");
+    ASSERT_TRUE(std::holds_alternative<std::string>(v.data));
+    EXPECT_EQ(std::get<std::string>(v.data), "hello");
+  }
+
+  TEST(Evaluator, BoolLiterals) {
+    EXPECT_TRUE(std::get<bool>(runScript("true;").data));
+    EXPECT_FALSE(std::get<bool>(runScript("false;").data));
+  }
+
+  TEST(Evaluator, NullLiteral) {
+    EXPECT_TRUE(std::holds_alternative<std::monostate>(runScript("null;").data));
+  }
+
+} // namespace
