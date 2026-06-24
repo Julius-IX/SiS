f0530e26443a5d770b32d77a3ca5690e932caea0
Julius Schutte <Julius.Schutte@gmail.com>
2026-06-25 00:53:21 +0200

    feat(val): update toString return format for Arrays
    instead of returning [0: val, 1: val, 2: val] if all keys are numbers and thus a normal array, it simplifies to [1, 2, 3]

--- src/Value.cpp
+++ src/Value.cpp
@@ -1,4 +1,5 @@
 #include <Value.h>
+#include <algorithm>
 #include <spdlog/fmt/fmt.h>
 
 namespace eval {
@@ -46,13 +47,25 @@ namespace eval {
         } else if constexpr (std::is_same_v<T, Array>) {
           std::string out = "[";
           if (v) {
-            bool first = true;
-            for (const auto& [key, value] : v->elements) {
-              if (!first) out += ", ";
-              first = false;
-              out += key.toString();
-              out += ": ";
-              out += value.toString();
+            bool no_table_entries = std::all_of(v->elements.begin(), v->elements.end(), [](const auto& p) {
+              return std::holds_alternative<double>(p.first.data);
+            });
+            if (no_table_entries) {
+              bool first = true;
+              for (const auto& [key, value] : v->elements) {
+                if (!first) out += ", ";
+                first = false;
+                out += value.toString();
+              }
+            } else {
+              bool first = true;
+              for (const auto& [key, value] : v->elements) {
+                if (!first) out += ", ";
+                first = false;
+                out += key.toString();
+                out += ": ";
+                out += value.toString();
+              }
             }
           }
           out += "]";
